39e262b45d118933cf9aaa43a4cab651fb53697e
MrTroll28 <{nguyenoke123@gmail.com}>
2025-04-02 19:28:41 +0700

    2. API overview done

--- Chefify/src/components/Admin/OverViewCard.jsx
+++ Chefify/src/components/Admin/OverViewCard.jsx
@@ -4,13 +4,13 @@ import Button1529 from "../../assets/Lab_05/Button 1529.png";
 import Button1530 from "../../assets/Lab_05/Button 1530.png";
 
 const buttonImages = {
-    Button1509: Button1509,
-    Button1529: Button1529,
-    Button1530: Button1530,
+    "Button1509": Button1509,
+    "Button1529": Button1529,
+    "Button1530": Button1530,
 };
 
 const OverViewCard = ({ title, amount, change, isIncrese, color, buttonValue }) => {
-    const buttonImg = buttonImages[buttonValue];
+    const buttonImg = buttonImages[buttonValue] || null;
 
     return (
         <div className="p-4 border rounded-lg shadow" style={{ backgroundColor: color }}>
@@ -19,7 +19,7 @@ const OverViewCard = ({ title, amount, change, isIncrese, color, buttonValue }) 
             <h3 className="text-lg font-bold">{title}</h3>
             <p className="text-2xl font-semibold">{amount}</p>
             </div>
-            {<img src={buttonImg} alt="button icon" className="w-10 h-10" />}
+            <button className='hover:scale-105' onClick={() => alert("đã ấn vào")}>{buttonImg && <img src={buttonImg} alt="button icon" className="w-10 h-10" />}</button>
         </div>
         <div>
             <p className={`text-sm ${isIncrese ? "text-green-500" : "text-red-500"}`}>
