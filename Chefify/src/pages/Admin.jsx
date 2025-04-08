537d911e0928f692bccd330bfa3fd8e5135f25de
MrTroll28 <{nguyenoke123@gmail.com}>
2025-04-08 19:46:35 +0700

    6. React Router 7. SPA -Single Application Page

--- Chefify/src/pages/Admin.jsx
+++ Chefify/src/pages/Admin.jsx
@@ -1,20 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import DataTable from "../components/Admin/DataTable";
 import SlideBar from "../components/Admin/SlideBar";
 import OverView from "../components/Admin/OverView";
-import DataTable from "../components/Admin/DataTable";
+import PageData from "../components/Admin/PageData";
 
 const Admin = () => {
   return (
     <div className="grid grid-cols-12 min-h-screen">
-        <SlideBar />
-        <div className="col-span-10 p-6">
-            <OverView />
-            <div className="mt-6">
-                <h2 className="text-2xl font-bold text-pink-500">Data Table</h2>
-                <DataTable />
-            </div>
+      <SlideBar />
+      <div className="col-span-10 p-6">
+        <OverView />
+        <div className="mt-6">
+          <Routes>
+            <Route path="/" element={<DataTable />} />
+            <Route path="/project" element={<PageData name="Project" />} />
+            <Route path="/dashboard" element={<PageData name="Dashboard" />} />
+            <Route path="/settings" element={<PageData name="Setting" />} />
+            <Route path="/profile" element={<PageData name="Profile" />} />
+          </Routes>
         </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Admin
+export default Admin;
