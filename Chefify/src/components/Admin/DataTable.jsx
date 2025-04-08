3490a8b5dee9c1a059cd24f7b7f1cbfbd1eacf37
MrTroll28 <{nguyenoke123@gmail.com}>
2025-04-08 20:53:43 +0700

    10. Add user --> Modal + API post

--- Chefify/src/components/Admin/DataTable.jsx
+++ Chefify/src/components/Admin/DataTable.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import edit from "../../assets/Lab_05/create.png";
+import file from "../../assets/Lab_05/File text 1.png";
+import upload from "../../assets/Lab_05/Move up.png";
+import dowload from "../../assets/Lab_05/Download.png";
 import EditModal from "./EditModal";
 
 const columns = (handleEditClick) => [
@@ -48,6 +51,7 @@ const DataTable = () => {
     const [data, setData] = useState([]);
     const [modalOpen, setModalOpen] = useState(false);
     const [selectedRow, setSelectedRow] = useState(null);
+    const [isAdd, setIsAdd] = useState(false); // true: Edit (PUT), false: Add (POST)
 
     useEffect(() => {
         fetch("https://67c81790c19eb8753e7c341f.mockapi.io/dataTable")
@@ -64,6 +68,7 @@ const DataTable = () => {
     const handleCloseModal = () => {
         setModalOpen(false);
         setSelectedRow(null);
+        setIsAdd(false);
     };
 
     const handleSave = async (updatedRow) => {
@@ -95,14 +100,51 @@ const DataTable = () => {
         }
     };    
 
+    const handleAdd = async (newRow) => {
+        try {
+          const res = await fetch("https://67c81790c19eb8753e7c341f.mockapi.io/dataTable", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newRow),
+          });
+          const result = await res.json();
+          setData(prev => [...prev, result]);
+          setModalOpen(false);
+        } catch (err) {
+          console.error("POST failed", err);
+        }
+        setIsAdd(false);
+    };
+
     return (
         <div style={{ height: 400, width: "100%" }}>
+            <div className="flex justify-between items-center mb-4">
+                <div className="flex items-center gap-4">
+                    <img src={file} alt="file" />
+                    <h2 className="font-bold text-[20px]">Detailed report</h2>
+                </div>
+                <div className="flex items-center gap-4">
+                    <button className="border-2 border-pink-500 py-[3px] px-2 rounded text-pink-500 flex gap-2 hover:scale-103"><img src={dowload} alt="dowload" />Export</button>
+                    <button className="border-2 border-pink-500 py-[3px] px-2 rounded text-pink-500 flex gap-2 hover:scale-103" 
+                    onClick={() => {
+                        setSelectedRow({
+                          name: "",
+                          company: "",
+                          value: "",
+                          date: "",
+                          status: "New",
+                        });
+                        setIsAdd(true);
+                        setModalOpen(true);
+                      }}><img src={upload} alt="upload" />Import</button>
+                </div>
+            </div>
             <DataGrid rows={data} columns={columns(handleEditClick)} pageSize={5} checkboxSelection />
             <EditModal
                 isOpen={modalOpen}
                 onClose={handleCloseModal}
                 rowData={selectedRow}
-                onSave={handleSave}
+                onSave={isAdd ? handleAdd : handleSave}
             />
         </div>
     );
