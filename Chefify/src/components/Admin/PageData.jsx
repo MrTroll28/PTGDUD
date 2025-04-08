3bf711f8b08282cc9bd7d0f8dbd83d65f7214d4e
MrTroll28 <{nguyenoke123@gmail.com}>
2025-04-08 19:46:35 +0700

    6. React Router 7. SPA -Single Application Page

---
+++ Chefify/src/components/Admin/PageData.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageData = ({ name }) => {
+  return (
+    <div>
+      <h1 className='text-center font-bold text-xl'>{name}</h1>
+    </div>
+  );
+};
+
+export default PageData;
--- Chefify/src/components/Admin/SlideBar.jsx
+++ Chefify/src/components/Admin/SlideBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "../../assets/Lab_05/Image 1858.png";
 import Square from "../../assets/Lab_05/Squares four 1.png";
 import Folder from "../../assets/Lab_05/Folder.png";
@@ -8,12 +9,13 @@ import Messages from "../../assets/Lab_05/Chat.png";
 import Integrations from "../../assets/Lab_05/Code.png";
 
 const menuItems = [
-    { name: "Dashboard", icon: Square },
-    { name: "Projects", icon: Folder },
-    { name: "Teams", icon: Groups },
-    { name: "Analytics", icon: Analytics },
-    { name: "Messages", icon: Messages },
-    { name: "Integrations", icon: Integrations },
+    { name: "Dashboard", icon: Square, link: "/" },
+    { name: "Projects", icon: Folder, link: "/project" },
+    { name: "Settings", icon: Square, link: "/settings" },
+    { name: "Profile", icon: Groups, link: "/profile" },
+    { name: "Analytics", icon: Analytics, link: "/analytics" },
+    { name: "Messages", icon: Messages, link: "/messages" },
+    { name: "Integrations", icon: Integrations, link: "/integrations" },
 ];
 
 const SlideBar = () => {
@@ -24,16 +26,15 @@ const SlideBar = () => {
             <img src={logo} alt="Logo" />
             <nav className="mt-4">
                 <ul>
-                    {menuItems.map(({ name, icon }) => (
-                        <li
-                            key={name}
-                            onClick={() => setActiveItem(name)}
-                            className={`flex items-center gap-4 p-2 cursor-pointer 
-                                hover:bg-pink-500 hover:text-white hover:rounded 
-                                ${activeItem === name ? "bg-pink-500 text-white rounded" : ""}`}
-                        >
-                            <img src={icon} alt={name} />
-                            {name}
+                    {menuItems.map(({ name, icon, link }) => (
+                        <li key={name} className="mb-2">
+                            <Link to={link} onClick={() => setActiveItem(name)}
+                              className={`flex items-center gap-4 p-2 hover:bg-pink-500 hover:text-white hover:rounded 
+                              ${activeItem === name ? "bg-pink-500 text-white rounded" : ""}`}
+                            >
+                              <img src={icon} alt={name} />
+                              {name}
+                            </Link>
                         </li>
                     ))}
                 </ul>
